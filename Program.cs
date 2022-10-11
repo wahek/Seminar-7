2f69ab6d80793aa3cea02078303b58e5bd291cd6
wahek <wahek1999@mail.ru>
2022-10-11 11:26:22 +0300

    Первая задача

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47.  Задайте двумерный массив размером m*n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 7 -2 -85
+// 1 -3 9 -9
+// 8 7 -7 9
+
+Console.WriteLine("Введите количество строк");
+int row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов");
+int column = int.Parse(Console.ReadLine()!);
+
+PrintArray(GetArray(row, column, -99, 99));
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+	return array;
+}
+
+void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+        	Console.WriteLine();
+        }
+	}
+
